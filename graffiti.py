bd13f670ff375078c77eb2ffb38635b384bc66a6
Christopher Lacy-Hulbert <clacy@yahoo-inc.com>
2013-08-01 23:57:00 +0100

    Not yet working executable for A-P-A-D

--- graffiti.py
+++ graffiti.py
@@ -5,7 +5,9 @@
 import json
 import time
 import math
+import subprocess
 
+tableau = "/Users/clacy/Development/web/tableau/README.md"
 toPrint = "GRAFITTI"
 
 fontConfig = json.loads(open('font.json').read())
@@ -26,20 +28,20 @@ commit();
 
 while True:
 	commit()
-	time.sleep(3600)   # 1 hour
+	time.sleep(3600)
 
-def commit:
+def commit():
 	dayOfWeek = time.strftime("%w") # int value
 	today = int(time.time())/86400
 
 	# check lastCommitDay
-	if( today == lastCommitDay ){
-		continue
-	}elif( dayOfWeek == 1 || dayOfWeek == 7){
+	if today == lastCommitDay:
+		# do nothing
+	elif dayOfWeek == 1 or dayOfWeek == 7:
 		# don't commit on monday or sunday 
 		# todo - make this fontConfig (fontHeight) aware
-		continue;
-	}else{
+		# do nothing
+	else:
 		# determine whether commit should happen today
 		daysSinceStart = today - startDay
 		currentChar = daysSinceStart / fontWidth	
@@ -47,22 +49,11 @@ def commit:
 						     # daysSinceStart: 32->7 33->7 34->7 35->8 36->8 37->8
 		currentChar = toPrint[currentChar]   # say, 'R' in GRAFFITI
 		columnInDay = daysSinceStart - ((currentChar-1)*5)
-		if( fontConfig['font']['letters'][currentChar][columnInDay] == 1){
-			subprocess.call()
-		}
+		if( fontConfig['font']['letters'][currentChar][columnInDay] == 1):
+			with open(tableau, "a") as f:
+				f.write(" ")
+			subprocess.call(["git","commit",tableau,"-m","pixel"])
+			subprocess.call(["git","push", "origin", "master"])
 
 		# prevent any more commit attempts today
 		lastCommitDay = today;
-	}
-
-
-#canStart = true
-
-#for day in firstChar[cols][1]   #firstChar->cols['1'] as day
-#	if day == 0
-#		keep going
-#	
-#		if day < dayOfWeek
-#			keep going
-#		
-#print "today's date is ",startDay
