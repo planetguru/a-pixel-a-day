11e5e39a7c663bd0d9ffba42eea5bfa01b51009c
clacy <clacy@yahoo-inc.com>
2014-06-24 20:23:34 +0100

    added ton of logging and fixed type of dayOfWeek check

--- graffiti.py
+++ graffiti.py
@@ -13,15 +13,21 @@ import os
 import pprint
 
 tableauFile = "tableau.txt"
-#tableauDir = "/export/a-pixel-a-day/" 
-tableauDir = "/Users/clacy/Development/web/a-pixel-a-day/"
+tableauDir = "/export/a-pixel-a-day/" 
+#tableauDir = "/Users/clacy/Development/web/a-pixel-a-day/"
 tableau = tableauDir + tableauFile
 toPrint = "&YAHOO"; # use & for testing. It is all '1's
+
+# prepare a basic log file
+f = open('/tmp/pad-log.txt', 'a')
+f.write("\nscript has started")
+f.close()
 
 fontConfig = json.loads(open('font.json').read())
 fontWidth = int(fontConfig['font']['fontWidth'])+1
 
-startDay = int(time.time())/86400 # days since epoch 
+startDay = int(time.time())/86400 # days since epoch where 86400 is no. seconds in a day
+#startDay = int(time.time())/60 # minute-long days since epoch where 86400 is no. seconds in a day
 today = startDay
 
 workingDays = [1,2,3,4,5]
@@ -33,9 +39,11 @@ workingDays = [1,2,3,4,5]
 def commit():
 	# update today to move forward from lastCommitDay on the turn of midnight
 	today = int(time.time()/86400) + 0 
+	#today = int(time.time()/60) + 0 
 
 	# check lastCommitDay
 	global lastCommitDay
+	global f
 	if today != lastCommitDay:
 		# determine whether commit should happen today
 		daysSinceStart = today - startDay
@@ -55,12 +63,24 @@ def commit():
 		# only commit if the bit in this position is a 1
 		pprint.pprint(fontConfig['font']['letters'][currentChar][columnInChar][bit])
 		if( fontConfig['font']['letters'][currentChar][columnInChar][bit] == str(1)):
+			f = open('/tmp/pad-log.txt', 'a')
+			f.write("\nabout to make a commit")
+			f.close()
 			fo=open(tableau,"a")
 			fo.write("-")
 			fo.close();
 			os.chdir(tableauDir)
+			f = open('/tmp/pad-log.txt', 'a')
+			f.write("\nabout to commit to repo")
 			subprocess.call(['git', 'commit', '--allow-empty', '-m', '""', 'README.md'])
+			f.write("\nabout to push to repo")
 			subprocess.call(['git', 'push'])
+			f.write("\npush to repo done");
+			f.close()
+		else:
+			f = open('/tmp/pad-log.txt', 'a')
+			f.write("\nnot committing this time because bit in this position is ".fontConfig['font']['letters'][currentChar][columnInChar][bit])
+			f.close()
 
 		# prevent any more commit attempts today
 		lastCommitDay = today;
@@ -71,10 +91,27 @@ global dayOfWeek
 # day of week as int. Sunday is 0.
 dayOfWeek = time.strftime("%w") # int value
 commit()
+f = open('/tmp/pad-log.txt', 'a')
+f.write("\nfirst commit is done - about to enter while loop")
+f.close()
 
 # then drop into a loop, always checking lastCommitDay, which gets updated in the commit function
 while True:
 	dayOfWeek = time.strftime("%w") # int value
-	if( dayOfWeek in workingDays ):
+	f = open('/tmp/pad-log.txt', 'a')
+	f.write("\nin loop. day of week is "+str(dayOfWeek))
+	f.close()
+	
+	if( int(dayOfWeek) in workingDays ):
+		f = open('/tmp/pad-log.txt', 'a')
+		f.write("\nabout to enter commit loop")
+		f.close()
 		commit()
+	else:
+		print workingDays
+		f = open('/tmp/pad-log.txt', 'a')
+		f.write("\nday of week is not in workingdays, so sleeping")
+		f.close()
+		
 	time.sleep(3600) 
+	#time.sleep(10) 
